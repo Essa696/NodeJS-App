d583878043aebec900f0f2e67c819f41af432c7a
Essa <essa.ea68@gmail.com>
2024-01-16 21:37:52 +1100

    node-update

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 
-
+app.use(bodyParser.json())
 app.use("/", express.static(__dirname + "/" ))
+
+app.post("/save-task", function(req, res) {
+
+    const taskObj = req.body
+    console.log("saved task" , taskObj.task)
+    res.send({savedTask: taskObj.task})
+
+} )
+
+app.get("/get-tasks", function(req, res) {
+
+    const tasks = [
+        "learn E2E Dev",
+        "Learn DevOps"
+    ]
+    res.send({tasks: tasks})
+})
 
 function doc() {
 
